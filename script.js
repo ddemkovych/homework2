49846d8541931e1ab52db781c8f625284b3cecee
unknown <ddemkovych4@gmail.com>
2020-07-23 18:48:07 +0300

    home2

---
+++ script.js
@@ -0,0 +1,38 @@
+let sum = 0;
+
+let N = Math.floor(+prompt ('перше число'));
+ while(isNaN(N) || !N || !Number.isInteger(N))
+    N = +prompt('ціле перше число');
+     
+ 
+let M = Math.floor(+prompt ('друге число'));
+ while(isNaN(M) || N>=M || !M || !Number.isInteger(M))
+       M = +prompt('ціле друге число більше ніж перше число');
+
+ 
+let skipNumbers = confirm ('пропускаємо парні числа ? Ок-(Так) Скасувати -(Ні)');
+
+  while ( N <= M )
+{
+      if (skipNumbers)
+    {
+      if ( N % 2 ===0 )    
+      {
+    
+      N ++;
+      continue;
+
+      }    
+    }
+      sum += N;
+       N ++;
+};
+
+
+let results = `
+    <b><p>Перше число - ${N}</p>
+    <p>Друге число - ${M}</p>
+    <P>Результат - ${sum}</p></b>`
+document.querySelector( '.results' ).innerHTML = results;
+
+          
